a06f7ed14779ed8940ef301e87f6d55d1aa14387
John Wang <john29917958@gmail.com>
2015-11-01 00:25:54 +0800

    Pull out image helper.

---
+++ src/Helpers/ImageFlipper.java
@@ -0,0 +1,40 @@
+package Helpers;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+
+public class ImageFlipper {
+	private ImageFlipper() {
+		
+	}
+	
+	public static Image horizontalFlip(Image img) {
+		AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+		tx.translate(-img.getWidth(null), 0);
+		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		BufferedImage flippedImage = op.filter(toBufferedImage(img), null);
+		return flippedImage;
+	}
+	
+	public static BufferedImage toBufferedImage(Image img)
+	{
+	    if (img instanceof BufferedImage)
+	    {
+	        return (BufferedImage) img;
+	    }
+
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+
+	    // Return the buffered image
+	    return bimage;
+	}
+}
--- src/NotCrowdingOutRuoshi/Views/Player.java
+++ src/NotCrowdingOutRuoshi/Views/Player.java
@@ -1,23 +1,17 @@
 package NotCrowdingOutRuoshi.Views;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-
-import javafx.scene.input.KeyCode;
+import Helpers.ImageFlipper;
 
 public class Player extends JPanel implements KeyListener {
 	public static enum STATUS {
@@ -77,21 +71,13 @@ public class Player extends JPanel implements KeyListener {
         Image currentFrame = images[frame];
         
         if (direction == DIRECTION.LEFT) {
-        	currentFrame = getHorizontalFlippedImage(currentFrame);
+        	currentFrame = ImageFlipper.horizontalFlip(currentFrame);
         }
         
         g.drawImage(currentFrame, 0, 0, this);
         
         frame = (frame + 1) % images.length;
     }
-	
-	private Image getHorizontalFlippedImage(Image img) {
-		AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
-		tx.translate(-img.getWidth(null), 0);
-		AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-		BufferedImage flippedImage = op.filter(toBufferedImage(img), null);
-		return flippedImage;
-	}
 
 	private Image[] loadImage(String imgResDir) {
 		File f = new File(imgResDir);
@@ -107,25 +93,6 @@ public class Player extends JPanel implements KeyListener {
 		}
 		
 		return container;
-	}
-	
-	public static BufferedImage toBufferedImage(Image img)
-	{
-	    if (img instanceof BufferedImage)
-	    {
-	        return (BufferedImage) img;
-	    }
-
-	    // Create a buffered image with transparency
-	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-
-	    // Draw the image on to the buffered image
-	    Graphics2D bGr = bimage.createGraphics();
-	    bGr.drawImage(img, 0, 0, null);
-	    bGr.dispose();
-
-	    // Return the buffered image
-	    return bimage;
 	}
 	
 	private class ScheduleTask extends TimerTask {		
